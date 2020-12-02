384ef366819166fd1008491e6a7fdb0e0707fa12
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-12-01 17:04:49 -0700

    Add interactive choice to "gulp release" for version selection

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const bump = require('gulp-bump'),
 	git = require('gulp-git'),
 	gitStatus = require('git-get-status'),
 	gulp = require('gulp'),
-	jshint = require('gulp-jshint');
+	jshint = require('gulp-jshint'),
+	prompt = require('gulp-prompt');
 
 function getVersionFromPackage() {
 	return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
@@ -20,10 +21,31 @@ gulp.task('ensure-clean-working-directory', (cb) => {
 	});
 });
 
-gulp.task('bump-version', () => {
-	return gulp.src([ './package.json' ])
-		.pipe(bump({ type: 'patch' }))
-		.pipe(gulp.dest('./'));
+gulp.task('bump-choice', (cb) => {
+	const processor = prompt.prompt({
+		type: 'list',
+		name: 'bump',
+		message: 'What type of bump would you like to do?',
+		choices: ['patch', 'minor', 'major'],
+	}, (res) => {
+		global.bump = res.bump;
+
+		return cb();
+	});
+
+	return gulp.src(['./package.json']).pipe(processor);
+});
+
+gulp.task('bump-version', (cb) => {
+	exec(`npm version ${global.bump || 'patch'} --no-git-tag-version`, {
+		cwd: './'
+	}, (error) => {
+		if (error) {
+			cb(error);
+		}
+
+		cb();
+	});
 });
 
 gulp.task('commit-changes', () => {
@@ -50,6 +72,7 @@ gulp.task('create-tag', (cb) => {
 
 gulp.task('release', gulp.series(
 	'ensure-clean-working-directory',
+	'bump-choice',
 	'bump-version',
 	'commit-changes',
 	'push-changes',
